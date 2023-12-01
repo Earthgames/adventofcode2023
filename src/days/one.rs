986135a1a048fcd307d92aa63ff2f35441cae77c
Earthgames <earthgame_s@outlook.com>
2023-12-01 16:08:24 +0100

    day one part two

--- src/days/one.rs
+++ src/days/one.rs
@@ -21,5 +21,62 @@ pub fn run() {
         );
         total += local_total.parse::<u32>().unwrap();
     }
-    print!("{total}");
+    println!("{total}");
+}
+
+pub fn runtwo() {
+    let input = get_input(1);
+    let lines = input.as_str().lines();
+    let mut total: u32 = 0;
+    for line in lines {
+        let mut word: String = "".to_string();
+        let mut local_total: Vec<char> = vec![];
+
+        for char in line.chars() {
+            if char.is_ascii_digit() {
+                local_total.push(char);
+                word = "".to_string();
+            } else {
+                word.push(char);
+                if word.contains("one") {
+                    local_total.push('1');
+                    word.drain(0..(word.len() - 2));
+                } else if word.contains("two") {
+                    local_total.push('2');
+                    word.drain(0..(word.len() - 2));
+                } else if word.contains("three") {
+                    local_total.push('3');
+                    word.drain(0..(word.len() - 2));
+                } else if word.contains("four") {
+                    local_total.push('4');
+                    word = "".to_string();
+                } else if word.contains("five") {
+                    local_total.push('5');
+                    word.drain(0..(word.len() - 2));
+                } else if word.contains("six") {
+                    local_total.push('6');
+                    word.drain(0..(word.len() - 2));
+                } else if word.contains("seven") {
+                    local_total.push('7');
+                    word.drain(0..(word.len() - 2));
+                } else if word.contains("eight") {
+                    local_total.push('8');
+                    word.drain(0..(word.len() - 2));
+                } else if word.contains("nine") {
+                    local_total.push('9');
+                    word.drain(0..(word.len() - 2));
+                }
+            }
+        }
+        let local_total: String = format!(
+            "{}{}",
+            &local_total[0],
+            match local_total.last() {
+                Some(a) => a,
+                None => &local_total[0],
+            }
+        );
+        total += local_total.parse::<u32>().unwrap();
+    }
+    println!("{total}");
 }
--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,23 @@ mod days;
 #[clap(propagate_version = true)]
 struct Cli {
     /// The day
-    #[arg(short, long, default_value_t = 1)]
+    // #[clap(default_value_t = 1)]
+    #[arg(short, long)]
     day: u8,
+
+    /// part two
+    #[clap(default_value_t = false)]
+    #[clap(short, long)]
+    part_two: bool,
 }
 
 fn main() {
     let cli = Cli::parse();
     match &cli.day {
-        1 => days::one::run(),
+        1 => match cli.part_two {
+            false => days::one::run(),
+            true => days::one::runtwo(),
+        },
         _ => print!("day not found"),
     }
 }
